29a062b60d992dada23fe0317a0a70a794c72f3a
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-04 13:54:57 +0200

    make build

---
+++ index.46f9d794.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");t.style.position="absolute",t.style.left="50%",t.style.top="50%",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.46f9d794.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+spider.style.left = (wallWidth - spiderWidth) / 2 + "px";
+spider.style.top = (wallHeight - spiderHeight) / 2 + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
